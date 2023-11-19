fd8e90e9051df42c3b77c8ce2f44f582c5536fdc
vickyzhang7 <130918669+vickyzhang7@users.noreply.github.com>
2023-11-19 15:32:26 -0500

    Update place.js

--- backend/models/place.js
+++ backend/models/place.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
-
-
 const placeSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
